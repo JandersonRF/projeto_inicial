b0a2d519dfffa31fb59b2c356dbadb55e7244c1d
Janderson <fernandesjanderso@gmail.com>
2024-11-13 22:28:40 -0300

    meu primeiro commit

---
+++ main.py
@@ -0,0 +1,18 @@
+print ("Jornanda de Dados")
+
+# Variaveis
+idade = 37
+nome = "Janderson"
+
+# Print
+print(idade)
+
+# Soma
+nova_idade = idade + 1
+print(nova_idade)
+
+# Lista
+lista = [1,2,3,45]
+
+#Metodos
+lista_inventada = lista.reverse()
