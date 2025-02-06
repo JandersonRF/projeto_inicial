a913f64e827315f08f4ccdb11becab45429289b5
Janderson <fernandesjanderso@gmail.com>
2025-02-06 20:02:18 -0300

    adicionar aula 01

---
+++ Bootcamp-Python/aula_01.py
@@ -0,0 +1,21 @@
+# Crie um programa que usuário digite seu nome e retorne o número de caracteres
+
+nome = input("Digite seu nome :")
+print(nome)
+print(len(nome)) 
+
+print(3 + 5)
+print("Olá" + nome + "!") #nesta configuração junta string
+
+#Crie um programa onde o usuário digite dois valores e aplique a soma
+valor1 = int(input("Digite primeiro valor"))
+valor2 = int(input("Digite o segundo valor"))
+soma = valor1 + valor2 #nesta configuração soma inteiro
+print("Valor da soma é: ", soma)
+
+
+#print(len(input ("Digite seu nome: ")))
+#print("Seu nome tem essa quantudade de caracteres: ") + print(None)
+
+
+
